62c9dda74d9592ff69c65d89b8c72ba31a94fcdd
Péter Kovács <peter.kovacs0519@gmail.com>
2019-04-11 22:05:32 +0200

    LFLT-361 Sitemap should be exposed to LCFA via Bridge  - Moved sitemap model classes from Leaflet  - Added Jackson XML data format extension  - Aligned module descriptor

---
+++ src/main/java/hu/psprog/leaflet/api/rest/response/sitemap/Sitemap.java
@@ -0,0 +1,105 @@
+package hu.psprog.leaflet.api.rest.response.sitemap;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import hu.psprog.leaflet.api.rest.response.common.BaseBodyDataModel;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Sitemap model.
+ * Compatible with Google search engine.
+ * Generated model structure will be:
+ * <urlset>
+ *     <url>
+ *         <loc>absolute URL</loc>
+ *     </url>
+ *     <url>
+ *         <loc>absolute URL</loc>
+ *     </url>
+ *     ...
+ *     <url>
+ *         <loc>absolute URL</loc>
+ *     </url>
+ * </urlset>
+ *
+ * @see SitemapLocationItem
+ * @author Peter Smith
+ */
+@JacksonXmlRootElement(localName = "urlset")
+@JsonDeserialize(builder = Sitemap.SitemapBuilder.class)
+public class Sitemap extends BaseBodyDataModel {
+
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "url")
+    private List<SitemapLocationItem> sitemapLocationItemList;
+
+    public List<SitemapLocationItem> getSitemapLocationItemList() {
+        return sitemapLocationItemList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Sitemap sitemap = (Sitemap) o;
+
+        return new EqualsBuilder()
+                .append(sitemapLocationItemList, sitemap.sitemapLocationItemList)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(sitemapLocationItemList)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("sitemapLocationItemList", sitemapLocationItemList)
+                .toString();
+    }
+
+    public static SitemapBuilder getBuilder() {
+        return new SitemapBuilder();
+    }
+
+    /**
+     * Builder for {@link Sitemap}.
+     */
+    public static class SitemapBuilder {
+
+        private Sitemap sitemap;
+
+        private SitemapBuilder() {
+            this.sitemap = new Sitemap();
+            this.sitemap.sitemapLocationItemList = new LinkedList<>();
+        }
+
+        public SitemapBuilder withLocation(String location) {
+            this.sitemap.sitemapLocationItemList.add(new SitemapLocationItem(location));
+            return this;
+        }
+
+        public SitemapBuilder withSitemapLocationItemList(List<SitemapLocationItem> sitemapLocationItemList) {
+            this.sitemap.sitemapLocationItemList = sitemapLocationItemList;
+            return this;
+        }
+
+        public Sitemap build() {
+            return this.sitemap;
+        }
+    }
+
+}
